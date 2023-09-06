7211dfaae9217f8b5a32980e51521e25f68ef524
Tech Monk <113084209+techmonk000@users.noreply.github.com>
2023-09-06 23:48:12 +0530

    Update area2.c

--- area2.c
+++ area2.c
@@ -1,26 +1,27 @@
+// This is used to print area of circle or volume of cylinder according to user's choice
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-    int option;
+    int option; // takes choice from user
     const float pi = 3.14;
-    float radius, area, height, volume;
+    float radius, area, height, volume; // creates variables for radius , height , area and volume of circle and cylinder
     printf("Enter 1 for area of circle and 2 for cylinder volume ");
-    scanf("%d", &option);
+    scanf("%d", &option); // user input of choice
     if (option == 1)
     {
         printf("Enter radius of circle");
         scanf("%f", &radius);
-        area = pi * pow(radius, 2);
+        area = pi * pow(radius, 2); // it calculates the area using formula for it
         printf("The area of circle is %0.2f", area);
     }
     else if (option == 2)
     {
         printf("Enter radius of cylinder");
         scanf("%f", &radius);
-        printf("Enter height of cylinder");
+        printf("Enter height of cylinder"); // takes height as additional input
         scanf("%f", &height);
-        volume = pi * pow(radius, 2) * height;
+        volume = pi * pow(radius, 2) * height; // it calculates volume of cylinder using its formula
         printf("The volume of cylinder is %0.2f", volume);
     }
     else {
